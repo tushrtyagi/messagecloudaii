d2ddc5a3a5f6291c871d069b4ea865502a804c69
mohammadparvez921 <parrymd786@gmail.com>
2024-04-15 16:14:05 +0530

    page almost done

---
+++ components/buildComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="build-component-div">
+    <div class="  border-bottom">
+        <i class="fa fa-picture-o" aria-hidden="true"></i>
+        Image
+    </div>
+    <div class="  border-bottom">
+        <i class="fa fa-file-text-o" aria-hidden="true"></i>
+        Text
+    </div>
+    <div class="  border-bottom">
+        <i class="fa fa-columns" aria-hidden="true"></i>
+        Columns
+    </div>
+    <div class="  border-bottom">
+        <i class="fa fa-file-image-o" aria-hidden="true"></i>
+        Image & Text
+    </div>
+
+</div>
+       
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        
+      };
+    },
+  };
+  </script>
+  
+  <style scoped lang="scss">
+    .build-component-div{
+         width: 100%;
+    }
+  </style>
+  
---
+++ components/singleSenderDetailsForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="w-100 main-div bg-white">
+    <form>
+      <label >Single Send Name *</label>
+      <input type="text" class="input-tag w-100 " />
+
+      <label>From Sender *</label>
+
+      <select required class="w-100 input-tag mb-1 " >
+        <option value="" disabled selected hidden>Select Sender</option> 
+          <option>test@gmail.com</option>
+          <option>test2@gmail.com</option>
+      </select>
+
+      <label>Subject</label>
+      <input type="text" class="input-tag w-100" />
+
+      <label>Preheader</label>
+      <input type="text" class="input-tag w-100" />
+
+      <hr />
+      <p class="mb-1">RECIPIENTS</p>
+      <hr />
+      <label>Send To*</label>
+      <select required class="w-100 input-tag mb-2">
+        <option value="" disabled selected hidden>Select recipients</option> 
+        <option>test@gmail.com</option>
+        <option>test2@gmail.com</option>
+      </select>
+
+      <label>Unsubscribe group*</label>
+      <select required class="w-100 input-tag mb-2" id="floatingInput">
+        <option value="" disabled selected hidden>Select an unsubscribe group</option> 
+        <option>test@gmail.com</option>
+        <option>test2@gmail.com</option>
+      </select>
+      <hr />
+      <p class="mb-1 ">SCHEDULE</p>
+      <hr />
+      <label>Schedule *</label>
+      <select class="w-100 input-tag mb-2" id="floatingInput">
+        <option value="" disabled selected hidden>Send immediately</option> 
+        <option>test@gmail.com</option>
+        <option>test2@gmail.com</option>
+      </select>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/colors.scss";
+@import "../assets/scss/style.scss";
+
+.input-tag {
+  outline: 0;
+  border-width: 0 0 2px;
+  border-radius: 0px;
+}
+.main-div {
+  label,
+  select {
+    font-size: 15px;
+    color: #000000;
+    font-family: ' "Inter", sans-serif';
+  }
+  .floatingInput {
+    border-radius: 0;
+  }
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    text-indent: 1px;
+    text-overflow: "";
+    -webkit-border-radius: 0; 
+    border-radius: 0;
+    transition:ease-in 2s;
+  
+  }
+  p {
+    font-weight: 700;
+    background-color: #fff;
+    color: #000000;
+    font-family: ' "Inter", sans-serif';
+  }
+  
+}
+</style>
